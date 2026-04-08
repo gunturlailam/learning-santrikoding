72b4eabaa34356c43dbf6ed7842a0b49671b98e2
Guntur Lailam Yuro <gunturlailamyuro8@gmail.com>
2026-04-08 21:04:41 +0700

    feat: integrasi javascript dan html

---
+++ 01-html-dasar/script.js
@@ -0,0 +1,14 @@
+// document.getElementById('tombol').addEventListener('click',function() {
+//     alert('Halo dari file eksternal');
+// })
+
+// Variabel
+let nama = "Santri Koding";
+
+// Fungsi
+function sapa() {
+    console.log("Halo " + nama);
+}
+
+// Pemanggilan fungsi
+sapa();
